c2898eadf76ac4d8ec8f5a81ec7d6bce7df13a8e
= <=>
2022-06-07 16:30:51 +0400

    add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
